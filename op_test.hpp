69f3c17e1c951e88d1cf13b8f5266ffe4befe9d2
Cyoo9 <tyo002@ucr.edu>
2020-01-30 13:57:54 -0800

    google unit tests

--- op_test.hpp
+++ op_test.hpp
@@ -10,4 +10,20 @@ TEST(OpTest, OpEvaluateNonZero) {
     EXPECT_EQ(test->evaluate(), 8);
 }
 
+TEST(OpTest, OpStringifyNonZero) {
+	Op* test = new Op(7.3);
+	EXPECT_EQ(test->stringify(), "7.300000");
+}
+
+TEST(OpTest, OpEvaluateNonNumber) {
+	Op* test = new Op('a');
+	EXPECT_EQ(test->evaluate(), 97);
+}
+
+TEST(OpTest, OpStringifyNonNumber) {
+        Op* test = new Op('a');
+        EXPECT_EQ(test->stringify(), "97.000000");
+}
+
+
 #endif //__OP_TEST_HPP__
--- unit_test.cpp
+++ unit_test.cpp
@@ -1,6 +1,10 @@
 #include "gtest/gtest.h"
 
 #include "op_test.hpp"
+#include "add_test.hpp"
+#include "mult_test.hpp"
+#include "pow_test.hpp"
+
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
