0bf65a870e94d3fd0f5152118bcaea8b7d6f3d0b
Cyoo9 <tyo002@ucr.edu>
2020-01-30 11:27:35 -0800

    updated .hpp files and main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 #include "base.hpp"
+#include "add.hpp"
+#include "mult.hpp"
+#include "op.hpp"
+#include "pow.hpp"
+
 
 int main() {
     // This is a very basic main, and being able to correctly execute this main
