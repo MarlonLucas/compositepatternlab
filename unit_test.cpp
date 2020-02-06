f831806a9d9f0821ab46d6c08b98f417369d7f81
achen163 <achen163@ucr.edu>
2020-02-06 11:10:08 -0800

    Update unit test file

--- unit_test.cpp
+++ unit_test.cpp
@@ -4,6 +4,10 @@
 #include "add_test.hpp"
 #include "mult_test.hpp"
 #include "pow_test.hpp"
+#include "rand_test.hpp"
+#include "sub_test.hpp"
+#include "div_test.hpp"
+
 
 
 int main(int argc, char **argv) {
